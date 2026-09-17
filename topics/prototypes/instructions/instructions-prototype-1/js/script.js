6048dfac0e8e659d234823af17e8ace266ab508a
SiaChia <siailiopoulos@gmail.com>
2026-09-16 20:42:01 -0400

    Added a title, my name and  description for my prototype in java script.

--- topics/prototypes/instructions/instructions-prototype-1/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-1/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Peanut the Bunny
+ * Athanasia (Sia) Iliopoulos 
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This is a drawing of my pet rabbit Peanut. I will be drawing him by programming different shapes and colours. 
  */
 
 "use strict";
