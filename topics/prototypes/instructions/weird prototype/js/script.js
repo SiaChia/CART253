363de3ffa5b2088fa01280e8c8b2be788e265fd8
SiaChia <siailiopoulos@gmail.com>
2026-09-17 18:54:16 -0400

    I created the prototype file. I made the title for the README.md , javascript and index.html called "Weird Unsettling Face". I also added my name to all of them and a description to README.md and Javascript.

---
+++ topics/prototypes/instructions/weird prototype/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Weird Unsettling Face
+ * Athanasia (Sia) Iliopoulos
+ * 
+ * A weird unsettling face that can maybe be used as a logo for a horror game or something idk. 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
